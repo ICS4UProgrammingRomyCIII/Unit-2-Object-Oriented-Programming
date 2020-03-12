914364b871bad58a48c165eac9a24e72d5c4a7ce
Romy Chu III <s243855@ocsb.local>
2020-03-12 12:56:53 -0400

    https://www.youtube.com/watch?v=dQw4w9WgXcQ

---
+++ Day20_(Vehicle Class)/src/Vehicle.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+public class Vehicle {
+	// Variables
+	public String licensePlateNumber; // This is a public variable for the vehicle's license plate
+	public String vehicleColor;		  // This is a public variable for the vehicle's colour.
+	public int numDoors;			  // This is a public variable for the number of doors.
+	
+	private static int currentSpeed = 0;	// A private variable for the vehicles current speed.
+	private static int maxSpeed = 0;		// A private variable for the vehicles max speed.
+	public static ArrayList<String> out = new ArrayList<>();
+	
+	public int GetCurrentSpeed() {return currentSpeed;}		// A public method to get the current speed.
+	public int getMaxSpeed() {return maxSpeed;}				// A public method to get the max speed.
+	
+	static protected void Accelerate() { currentSpeed += 10; maxSpeed += 10; }	// A protected method tp accelerate the car.
+	static protected void Brake() {												// A private method to brake the car.
+		if (currentSpeed > 0) { currentSpeed -= 5;}								// If the current speed is greater than 0, decrease the speed by 5.			
+	}
+	public void FillAddCar(int numCar) {
+		// Adds all of the information the list of information.
+		String outTemp = String.format("Vehicle #%d : ", numCar) + "\n"
+						+ "License Plate : " + licensePlateNumber + "\n"
+						+ "Car Colour : " + vehicleColor + "\n"
+						+ "Number of Doors : " + numDoors + "\n"
+						+ "Current Speed : " + currentSpeed + "\n"
+						+ "Max Speed : " + maxSpeed + "\n\n";
+		out.add(outTemp);
+		
+		// Resets all of the variables
+		licensePlateNumber = null;
+		vehicleColor = null;
+		numDoors = 0;
+		currentSpeed = 0;
+		maxSpeed = 0;
+	}
+}
---
+++ Day20_(Vehicle Class)/src/Vehicle_Program.java
@@ -0,0 +1,72 @@
+/*
+ * Created by: Romy I. Chu III
+ * Created on: 12-March-2019
+ * Created for: ICS4U
+ * Day #20 (Vehicle Class)
+ * This program...
+*/
+
+import java.util.*;
+
+public class Vehicle_Program {
+	// Objects
+	static Scanner sc = new Scanner(System.in); // To read the user's input from the console. 
+	static Vehicle vehicle = new Vehicle(); 	 // New object for the Vehicle Class
+	
+	static // Variables
+	int numCar = 1;
+	static boolean firstTime = true;
+	
+	public static void main(String[] args) {
+		while(true) {
+			System.out.println("Add a Vehicle? y/N");
+			String addNew = sc.nextLine().toUpperCase(); // Get the letter.
+			
+			if(addNew.equals("Y")) { 			// If the letter is Y...
+				CreateVehicle();	// Call CreateVehicle.
+			} else { 						// Otherwise...
+				System.out.println("Print all info? y/N");// Ask if the user wants to print all of the info
+				String pAll = sc.nextLine();	// Get the letter.
+				pAll.toUpperCase();				// Change it to upper case.
+				if (pAll.equals("Y")) { for(String s : vehicle.out) { System.out.println(s); } }
+				break;
+			}
+		}
+	}
+	
+	static void CreateVehicle() {
+		// Gets the car's license plate from the user.
+		System.out.println("License Plate Number : ");
+		vehicle.licensePlateNumber = sc.nextLine();
+		
+		// Gets the car's colour from the user.
+		System.out.println("Car's Colour : ");
+		vehicle.vehicleColor = sc.nextLine();
+		
+		// This try catch will ensure that the user only inputs an integer.
+		while(vehicle.numDoors == 0) {
+			System.out.println("Number of Doors : ");							  // Gets the number of car doors from the user.
+			try {vehicle.numDoors = Integer.parseInt(sc.nextLine()); }  		  // Parse it as an integer.
+			catch(Exception e) {System.out.println("Please enter an integer."); } // If the user didn't input and integer, tell them to enter a new one.
+		}
+		
+		for (int i = 0; i < 10; i++ ) {
+			System.out.println("Accelerate (A) or Brake (B)?");
+			String letter = sc.nextLine();	// Get the letter.
+			letter.toUpperCase();			// Change it to upper case.
+			// Check Whether or not the user inputed a valid letter.
+			while(letter.equals("A") == false && letter.equals("B") == false ) {
+				System.out.println("ONLY enter A or B, to accelerate or brake!! "); // Tell the user that they did not input a valid character.S
+				letter = sc.nextLine();		// Get the letter.
+				letter.toUpperCase();		// Change it to upper case.
+			}
+			if(letter.equals("A")) 		// If the letter is A...
+				Vehicle.Accelerate();	// Call accelerate
+			else 						// Otherwise...
+				Vehicle.Brake();		// Call Brake.
+		}
+		System.out.println();	// Print a blank line
+		vehicle.FillAddCar(numCar);
+		numCar++; // Increment the numCar
+	}
+}
